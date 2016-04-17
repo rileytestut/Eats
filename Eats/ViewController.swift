bd00f334091577b2154a623dfba504b92f44049f
Riley Testut <riley@rileytestut.com>
2016-04-17 02:34:04 -0700

    Initial Commit

---
+++ Eats/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Eats
+//
+//  Created by Riley Testut on 4/17/16.
+//  Copyright © 2016 Spark SC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
