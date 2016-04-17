81aa687f3e95283f2b1a512b28afee8c5349c8ce
Riley Testut <riley@rileytestut.com>
2016-04-17 02:47:14 -0700

    Embeds MasterViewController in UINavigationController

--- Eats/MasterViewController.swift
+++ Eats/MasterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MasterViewController.swift
 //  Eats
 //
 //  Created by Riley Testut on 4/17/16.
@@ -8,21 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class MasterViewController: UIViewController
 {
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
